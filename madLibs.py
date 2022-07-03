a52ccd7f23f8e451f5997f8e0f3ab8d18c6512a3
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2022-07-03 20:14:28 +0530

    Create madLibs.py

---
+++ madLibs.py
@@ -0,0 +1,11 @@
+"""
+@author: Ayush Mazumdar
+This is a tutorial file
+"""
+color = input("Enter a color: ")
+pluralNoun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(pluralNoun + " are blue")
+print("I love " + celebrity)
