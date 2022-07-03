75192a30a41c1ba6eee77cd5f3441f3723749a85
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2022-07-03 18:57:05 +0530

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,23 @@
+"""
+@author: Ayush Mazumdar
+This is a tutorial file
+"""
+from math import *
+#importing the math functions
+print(-7.56)
+print(3 * (4.9 + 5))
+print(10 % 3)
+
+my_num=6
+print(my_num)
+print(str(my_num) + " is my favorite number") #convert number to string
+
+my_num = -9
+print(abs(my_num)) #give the absolute value of number
+print(pow(7,9)) #raise the first number to the power of second
+print(max(7,9)) #find the higher number
+print(min(7,9)) #find the lower number
+print(round(5.6)) #round the number
+print(floor(5.6)) #give the floor of the number
+print(ceil(5.6)) #give the ceiling of the number
+print(sqrt(64)) #give the square root
