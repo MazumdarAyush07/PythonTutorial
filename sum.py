5297544a797c81d7063a2e4ee8b23ce0a3bfe236
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2022-07-03 20:07:13 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,8 @@
+"""
+@author: Ayush Mazumdar
+This is a tutorial file
+"""
+num1 =  input("Enter a number: ")
+num2 =  input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
